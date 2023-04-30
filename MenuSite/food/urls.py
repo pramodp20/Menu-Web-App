c9343f54513e716ad63ddfa54dc040321ad668f8
Pramod Pasala <131377812+pramodp20@users.noreply.github.com>
2023-04-30 10:29:44 +0000

    added detailed page

--- MenuSite/food/urls.py
+++ MenuSite/food/urls.py
@@ -1,7 +1,8 @@
 from . import views
 from django.urls import path
-
+app_name='food'
 urlpatterns = [
     path('',views.index,name='index'),
-    path('item/',views.item,name='item')
+    path('item/',views.item,name='item'),
+    path('<int:item_id>/',views.detail,name='detail')
 ]
--- MenuSite/food/views.py
+++ MenuSite/food/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Index Page')
+    item_list=Item.objects.all()
+    return render(request,'food/index.html',{
+        'items' : item_list
+    })
 
 def item(request):
     return HttpResponse('This ia an item view')
+
+def detail(request,item_id):
+    item=Item.objects.get(pk=item_id)
+    return render(request,'food/detail.html',{
+        'item' : item
+    })
